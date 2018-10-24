f71208adf70ef47d65710bee7d7fcd1f709af121
Michael Pantalleresco <36388893+MichaelPantallerescoMYOB@users.noreply.github.com>
2018-10-24 13:58:47 +1100

    update field_spec.rb
    tests look good to me, just some small changes to make the tests smaller/cleaner

--- spec/field_spec.rb
+++ spec/field_spec.rb
@@ -3,12 +3,13 @@ require 'field'
 
 describe Field do
   context "When testing the Field class " do
-
+    # with the changes in the field.rb class these test will be a bit cleaner
     it 'should return 5 weeks when given 15 people and 1 starting fruit' do
-      field = Field.new(15, 1)
-      field.calculate_weeks
-      weeks = field.week
+      field = Field.new(15, 1) # can remove the number of people from the creation of the field
+      field.calculate_weeks # could remove this part
+      # weeks = field.week - can remove this part
       expect(weeks).to eq 5
+      # the above could become expect(field.calculate_weeks(15)).to eq 5
     end
 
     it 'should return 14 weeks when given 50 000 people and 1 starting fruit' do
