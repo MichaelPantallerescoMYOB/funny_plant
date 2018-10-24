d0411bbff3a226262e79c6e532fc39c264d00be7
Michael Pantalleresco <36388893+MichaelPantallerescoMYOB@users.noreply.github.com>
2018-10-24 13:51:16 +1100

    adding some comments to field.rb
    The code looks really good, just some small things that make it a little cleaner

--- lib/field.rb
+++ lib/field.rb
@@ -1,22 +1,23 @@
 require_relative 'funny_plant'
 
 class Field
-  attr_reader :target_harvest, :week
+  # attr_reader :target_harvest, :week I would remove this section as you are only seeking the week from the calucation
 
   def initialize(people_to_feed, seeds)
     @plants = plant(seeds)
-    @target_harvest = people_to_feed
+    # @target_harvest = people_to_feed Can be passed into the calculate_weeks method
     @week = 1
   end
 
-  def calculate_weeks
+  def calculate_weeks(target_harvet) # pass the target harvet straight into the method 
     fruits_produced = 0
     fruits_produced = harvest
-    until fruits_produced > @target_harvest
+    until fruits_produced > target_harvest
       increment_week
       fruits_produced = harvest
       plant(fruits_produced)
     end
+    @week # This will allow you to return the week from within the method, week isn't really apart of the feild class.
   end
 
   private
@@ -52,6 +53,10 @@ class Field
   end
 end
 
+# this would become 
+# field = Field.new(1)
+# puts field.calulate_weeks(15)
+# -----------------------------
 # field = Field.new(15, 1)
 # field.calculate_weeks
 # puts field.week
